f676d117a46b0665dc0092a051024c7297c961d8
Yoshiyuki Hamada <yoshiyuki.hamada@i-studio.co.jp>
2018-08-21 19:38:07 +0900

    ejsのasync / await化
    メソッドを分割
    変数名も見直し

--- script/ejs.js
+++ script/ejs.js
@@ -13,50 +13,75 @@ const tool = require('./tool');
 const obj = {};
 
 
-// EJSのコンパイル & ファイルコピー
-obj.compile = async (filePath) => {
+/**
+ * EJSの個別コンパイル & ファイルコピー
+*/
+obj.compile = async (ejsPath) => {
 	try{
-		const destPath = tool.convertEJSDirnameToDestDirname(filePath);
-		const dest = tool.convertEJSPathToDestPath(filePath);
-
-		// async / awaitバージョン
-		const dir = await (() => {
-			mkdirp(destPath);
-		})();
-		const ejsFile = await (() => {
-			return readFile(filePath, config.encoding);
-		})();
-		const htmlFile = await (() => {
-			return ejs.compile(ejsFile, {
-				filename: filePath
-			});
-		})();
-		const _writeFile = await (() => {
-			return writeFile(dest, htmlFile({
-				// browsersyncの非公開メソッド
-				// immutable.jsが使用されているのでtoJSで元に戻す
-				// 絶対パスのために必要
-				// ejsテンプレートに
-				// {
-				// 	local: 'http://localhost:\d\d\d\d',
-				//	external: 'http://(IP):\d\d\d\d'
-				// }
-				// が渡る
-				urls: bs.instance.utils.getUrlOptions(bs.instance.options).toJS(),
-				env: process.argv[2],
-			}));
-		})();
+		const dir = await obj.createDestDirectory(ejsPath);
+		const ejsFile = await obj.readEJSFile(ejsPath);
+		const compileEJSFile = await obj.compileEJSFile(ejsPath, ejsFile);
+		const writeFile = await obj.writeHTMLFile(ejsPath, compileEJSFile);
 	}catch(error){
 		console.log('error');
 		console.log(error);
 	}
 };
 
+/**
+ * EJSのPathからDestディレクトリを作成
+*/
+obj.createDestDirectory = (ejsPath) => {
+	const destDirname = tool.convertEJSDirnameToDestDirname(ejsPath);
+
+	return mkdirp(destDirname);
+};
+
+/**
+ * EJSファイルの読み込み
+*/
+obj.readEJSFile = (ejsPath) => {
+	return readFile(ejsPath, config.encoding);
+};
+
+/**
+ * EJSファイルのコンパイル
+*/
+obj.compileEJSFile = (ejsPath, ejsFile) => {
+	return ejs.compile(ejsFile, {
+		filename: ejsPath
+	});
+};
+
+/**
+ * HTMLファイルの書き込み
+*/
+obj.writeHTMLFile = (ejsPath, compileEJSFile) => {
+	const destPath = tool.convertEJSPathToDestPath(ejsPath);
+
+	return writeFile(destPath, compileEJSFile({
+		// browsersyncの非公開メソッド
+		// immutable.jsが使用されているのでtoJSで元に戻す
+		// 絶対パスのために必要
+		// ejsテンプレートに
+		// {
+		// 	local: 'http://localhost:\d\d\d\d',
+		//	external: 'http://(IP):\d\d\d\d'
+		// }
+		// が渡る
+		urls: bs.instance.utils.getUrlOptions(bs.instance.options).toJS(),
+		env: process.argv[2],
+	}));
+};
+
+/**
+ * EJSの全体コンパイル & ファイルコピー
+*/
 obj.dest = async () => {
 	try{
-		const files = await glob(config.ejs.src);
-		return files.map((file) => {
-			return obj.compile(file);
+		const ejsPathes = await glob(config.ejs.src);
+		return ejsPathes.map((ejsPath) => {
+			return obj.compile(ejsPath);
 		});
 	}catch(error){
 		console.log('error');
