68d24d8304a119396f4832df354578ce9671e975
Yoshiyuki Hamada <yoshiyuki.hamada@i-studio.co.jp>
2018-08-22 17:11:32 +0900

    sassのasync / await化
    Promiseを隠蔽してasyncとawaitを使う

--- script/sass.js
+++ script/sass.js
@@ -1,44 +1,54 @@
+'use strict';
 const config = require('./config');
+const util = require('util');
 const path = require('path');
 const fs = require('fs');
 const sass = require('node-sass');
-const mkdirp = require('mkdirp');
-const glob = require('glob');
+const mkdirp = util.promisify(require('mkdirp'));
+const glob = util.promisify(require('glob'));
+const readFile = util.promisify(fs.readFile);
+const writeFile = util.promisify(fs.writeFile);
 const autoprefixer = require('autoprefixer');
 const obj = {};
 
 
-
 // SASSのコンパイル & ファイルコピー
-obj.compile = (filePath) => {
-	const extension = path.extname(filePath);
-	const filename = path.basename(filePath, extension);
-	const dirname = config.dest + path.dirname(filePath).replace(config.src, '') + '/';
-	const dest = dirname + filename + '.css';
+obj.compile = async (filePath) => {
+	try{
+		const extension = path.extname(filePath);
+		const filename = path.basename(filePath, extension);
+		const dirname = `${config.dest}${path.dirname(filePath).replace(config.src, '')}/`;
+		const destPath = `${dirname}${filename}.css`;
+		const render = util.promisify(sass.render);
 
-	mkdirp(dirname, (err) => {
-		if(err) {
-			console.log('sass');
-			console.log(err);
-			return;
-		}
-
-		const file = fs.readFileSync(filePath, config.encoding);
-		const data = sass.renderSync({
+		// ディレクトリの作成
+		const dir = await mkdirp(dirname);
+		// sassファイルの読み込み
+		const file = await readFile(filePath, config.encoding);
+		// sassのコンパイル
+		const data = await render({
 			data: file
 		});
-		const css = autoprefixer.process(data.css);
-
-		fs.writeFileSync(dest, css);
-	});
+		// vendorprefixの付与
+		const css = await autoprefixer.process(data.css);
+		// cssファイルの書き込み
+		const write = await writeFile(destPath, css);
+	}catch(error){
+		console.log('error');
+		console.log(error);
+	}
 };
 
-obj.dest = () => {
-	glob(config.sass.src, (err, files) => {
-		files.forEach((path, index) => {
-			obj.compile(path);
+obj.dest = async () => {
+	try{
+		const sassPathes = await glob(config.sass.src);
+		return sassPathes.map((sassPath) => {
+			obj.compile(sassPath);
 		});
-	});
+	}catch(error){
+		console.log('error');
+		console.log(error);
+	}
 };
 
 
