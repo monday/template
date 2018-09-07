b20df2cf96c2101eb7e2274e9d9545042923584a
Yoshiyuki Hamada <yoshiyuki.hamada@i-studio.co.jp>
2018-09-07 13:43:30 +0900

    改行調整
    2行開けから1行へ

--- script/delete.js
+++ script/delete.js
@@ -5,19 +5,15 @@ const tool = require('./tool');
 const obj = {};
 
 
-
 obj.exec = (filePath) => {
 	const expression = filePath ? tool.convertSrcToDest(filePath) : config.dest;
 
 	del.sync(expression);
 }
 
-
 // コマンドからの実行
 if(/delete\.js/.test(process.argv[1])){
 	obj.exec();
 }
-
-
 
 module.exports = obj;
