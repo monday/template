43e7ac043fab0577f1b41caa03c856fe8c246c72
Yoshiyuki Hamada <yoshiyuki.hamada@i-studio.co.jp>
2018-09-07 13:47:35 +0900

    コメント編集
    コメントスタイルを共通化

--- script/delete.js
+++ script/delete.js
@@ -5,13 +5,19 @@ const tool = require('./tool');
 const obj = {};
 
 
+/**
+ * filePathのファイルを削除する
+ * 引数なしの場合は全削除する
+*/
 obj.exec = (filePath) => {
 	const expression = filePath ? tool.convertSrcToDest(filePath) : config.dest;
 
 	del.sync(expression);
 }
 
-// コマンドからの実行
+/**
+ * コマンドから実行する
+*/
 if(/delete\.js/.test(process.argv[1])){
 	obj.exec();
 }
